40f8c0bdca8fb1d53deba095cab4e2c740bd2f40
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-04 10:49:24 -0700

    10-4-2019 get by id/ trying to workout bugs

--- projectRoute/ProjectRoute.js
+++ projectRoute/ProjectRoute.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const router = express.Router()
+
 const pj = require('../data/helpers/projectModel')
+
+const router = express.Router()
 
 // console.log(router)
 
 router.get('/', (req, res) => {
-    pj.get()
+    pj.find()
         .then(pro => {
             res.status(200).json(pro)
         })
@@ -13,15 +15,29 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/:id', (req, res) => {
+    const { id } =req.params
+
+    pj.find(id)
+        .then(pro => {
+            if(!id){
+                res.status(404).json({ error: "cannot find project" })
+            } else {
+                res.status(200).json(pro)
+            }
+        })
+        .catch(err => res.send(err))
+})
+
 
 router.post('/', (req, res) => {
     const newPro = req.body
-    
+    // console.log(newPro)
     if(!newPro.name && !newPro.description){
         return res.status(400).json({ errorMessage: "Name feild and Description feild required" })
     } else{
         pj.insert(newPro)
-            .then(pro => res.status(201).json(console.log(pro)))
+            .then(pro => res.status(201).json(pro))
             .catch(err => res.status(500).json({ error: "problem creating project" }))
     }
 })
